7325f8a6168a94e9144bf26379b92a286d3ec6db
Robert Vojcik <robert@vojcik.net>
2026-05-21 15:00:09 +0200

    fix commit tag ref propagation

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r") as fh:
 
 setup(
     name="vault-certificate-deploy",
-    version=os.getenv("CI_COMMIT_TAG", default="1.3.6"),
+    version=os.getenv("CI_ACTION_REF_NAME", default="0.0.1"),
     packages=find_packages(),
     install_requires=install_reqs,
     license="GPLv3",
